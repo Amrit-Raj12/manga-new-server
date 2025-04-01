e581e33a9bb95f3ac748f23fd83ce0c7e8c772e8
Amrit-Raj12 <amrit.raj1224@gmail.com>
2025-04-01 09:01:36 +0530

    New Manga Server

--- app/index.js
+++ app/index.js
@@ -2,10 +2,16 @@ const express = require("express");
 const app = express();
 const routes = require("./routes");
 const cors = require('cors');
+const path = require("path");
 
 app.use(express.json());
 app.use("/api", routes);
 app.use(cors());
+
+// Serve the HTML file on the home route
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 const port = 3000;
 app.listen(port, () => {
